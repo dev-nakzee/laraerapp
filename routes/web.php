995a14f496c70b1628cdf345bf212fe83379da71
nakzee <dev.nakzee@gmail.com>
2023-03-04 15:02:58 +0530

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use App\Http\Controllers;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('https://xeniuminfotech.in/');
 });
 
 Route::get('/admin', function () {
